afd8338361d6a548fca6124974cbc11b6ed5bb41
kle0247 <karolyn.le23@gmail.com>
2023-02-28 16:16:08 -0500

    add some functionality to search bar

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Search() {
+
+  const [term, setTerm] = useState('')
+  
+  const handleClick = () => {
+    //axios call to get search orgs - using in term
+    console.log('clicked from function')
+  }
+
   return (
     <div>
-        <input placeholder='search' type='text' />
-        <button type='submit'>Go</button>
+      <input placeholder='search for nonprofit' type='text' name='term' value={term} onChange={(ev) => setTerm(ev.target.value)}/>
+      <button type='click' onClick={ () => handleClick() }>Go</button>
     </div>
   )
 }
